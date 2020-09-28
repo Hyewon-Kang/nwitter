893c8a9e3c5b1a2195ba53627dc2e4720d6859ec
Hyewon-Kang <gpdnjs22@@>
2020-09-28 15:16:59 +0900

    upload image

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -4,10 +4,10 @@ import React, { useState } from "react";
 const Nweet = ({ nweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
   const [newNweet, setNewNweet] = useState(nweetObj.text);
-  const onDeleteClick = () => {
+  const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure you want to delete this nweet?");
     if (ok) {
-      dbService.doc(`nweets/${nweetObj.id}`).delete();
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
     }
   };
   const toggleEditing = () => setEditing((prev) => !prev);
